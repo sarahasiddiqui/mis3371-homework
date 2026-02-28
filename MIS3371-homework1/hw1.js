205ab51f8b75e04e8b8d7fbad991805054bfbf6c
sarahasiddiqui <sarahsiddiqui974@gmail.com>
2026-02-27 21:31:13 -0600

    Update hw1.js

--- MIS3371-homework1/hw1.js
+++ MIS3371-homework1/hw1.js
@@ -18,7 +18,10 @@ form.addEventListener('submit', function(e) {
     if(password !== confirmPassword) {
         errorMsg.textContent = "Passwords do not match!";
         return; 
-    } 
+    }
+
+    //Redirect to the thank you page
+    window.location.href = "thankyou.html";
     
     //reset the form
     form.reset();
